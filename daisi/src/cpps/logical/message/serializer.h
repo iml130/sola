885b4b39b396d0b9c528138183c6c6a601fe0efd
janagoe <jana.goedeke@iml.fraunhofer.de>
2023-05-30 11:36:02 +0200

    feat(daisi): adding logical messages and serializer

---
+++ daisi/src/cpps/logical/message/serializer.h
@@ -0,0 +1,36 @@
+// Copyright 2023 The SOLA authors
+//
+// This file is part of DAISI.
+//
+// DAISI is free software: you can redistribute it and/or modify it under the terms of the GNU
+// General Public License as published by the Free Software Foundation; version 2.
+//
+// DAISI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+// the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with DAISI. If not, see
+// <https://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: GPL-2.0-only
+
+#ifndef DAISI_CPPS_LOGICAL_MESSAGE_SERIALIZER_H_
+#define DAISI_CPPS_LOGICAL_MESSAGE_SERIALIZER_H_
+
+#include <string>
+#include <variant>
+
+#include "bid_submission.h"
+#include "call_for_proposal.h"
+
+namespace daisi::cpps::logical {
+
+using Message = std::variant<CallForProposal, BidSubmission>;  // placeholder
+
+std::string serialize(const Message &msg);
+
+Message deserialize(const std::string &msg);
+
+}  // namespace daisi::cpps::logical
+
+#endif
