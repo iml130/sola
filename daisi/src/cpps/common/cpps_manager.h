d4d9e9ba73595ed4c9326a88b046386ebb74f0ee
Lars Toenning <lars.toenning@iml.fraunhofer.de>
2023-09-05 15:40:22 +0200

    refactor(daisi): Adapt CPPS manager to new manager base class

--- daisi/src/cpps/common/cpps_manager.h
+++ daisi/src/cpps/common/cpps_manager.h
@@ -14,36 +14,28 @@
 //
 // SPDX-License-Identifier: GPL-2.0-only
 
-#ifndef DAISI_CPPS_MANAGER_H_
-#define DAISI_CPPS_MANAGER_H_
+#ifndef DAISI_CPPS_COMMON_CPPS_MANAGER_H_
+#define DAISI_CPPS_COMMON_CPPS_MANAGER_H_
 
-#include <deque>
-#include <optional>
 #include <queue>
 #include <unordered_map>
 
 #include "cpps/amr/amr_topology.h"
-#include "cpps/amr/model/amr_static_ability.h"
-#include "cpps/common/cpps_application.h"
-#include "cpps/common/cpps_logger_ns3.h"
-#include "cpps/logical/material_flow/material_flow_logical_agent.h"
-#include "manager/manager_old.h"
-#include "ns3/bridge-helper.h"
-#include "ns3/csma-helper.h"
-#include "ns3/internet-stack-helper.h"
-#include "ns3/ipv4-address-helper.h"
-#include "ns3/ipv4-static-routing-helper.h"
-#include "ns3/net-device-container.h"
-#include "ns3/nstime.h"
-#include "ns3/simulator.h"
+#include "manager/core_network.h"
+#include "manager/manager.h"
+#include "ns3/network-module.h"
 #include "scenariofile/cpps_scenariofile.h"
 
 namespace daisi::cpps {
 
-class CppsManager : public daisi::ManagerOld<CppsApplication> {
+class CppsManager : public daisi::Manager {
 public:
   explicit CppsManager(const std::string &scenario_config_file);
-  void setup() override;
+
+private:
+  void setupImpl() override;
+  GeneralScenariofile getGeneralScenariofile() const override { return scenario_; }
+  std::string getDatabaseFilename() const override;
 
   void initAMR(uint32_t index);
   void connectAMR(uint32_t index);
@@ -55,17 +47,13 @@ public:
   void scheduleMaterialFlow(const SpawnInfoScenario &info);
   void executeMaterialFlow(int index, const std::string &friendly_name);
 
-private:
   void initialSpawn();
 
   void setupNodes();
-  void setupNetworkEthernet();
-  void setupNetworkWifi();
 
-  void scheduleEvents() override;
+  void scheduleEvents();
 
-  uint64_t getNumberOfNodes() override;
-  std::string getDatabaseFilename() override;
+  uint64_t getNumberOfNodes() const;
 
   void spawnAMR(uint32_t amr_index, const AmrDescription &description, const Topology &topology);
 
@@ -91,9 +79,8 @@ private:
   // Nodes / Network
   ns3::NodeContainer amrs_;
   ns3::NodeContainer material_flows_;
-  ns3::NodeContainer access_points_;
 
-  ns3::NetDeviceContainer amr_sta_dev_;
+  CoreNetwork core_network_;
 };
 
 }  // namespace daisi::cpps
