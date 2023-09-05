be07801d07552f13f067bdfd75fe294c78359d4d
Lars Toenning <lars.toenning@iml.fraunhofer.de>
2023-09-05 15:40:22 +0200

    refactor(daisi): Adapt CPPS manager to new manager base class

--- daisi/src/cpps/common/cpps_manager.cpp
+++ daisi/src/cpps/common/cpps_manager.cpp
@@ -23,16 +23,18 @@
 #include <string>
 #include <unordered_map>
 
-#include "../src/logging/logger.h"
 #include "cpps/amr/model/amr_fleet.h"
+#include "cpps/amr/model/amr_static_ability.h"
 #include "cpps/amr/physical/amr_mobility_model_ns3.h"
 #include "cpps/amr/physical/amr_physical_asset.h"
-#include "cpps/logical/algorithms/algorithm_config.h"
+#include "cpps/common/cpps_application.h"
 #include "cpps/logical/amr/amr_logical_agent.h"
+#include "cpps/logical/material_flow/material_flow_logical_agent.h"
+#include "logging/logger_manager.h"
+#include "manager/sola_helper.h"
 #include "ns3/core-module.h"
+#include "ns3/internet-module.h"
 #include "ns3/mobility-helper.h"
-#include "ns3/wifi-module.h"
-#include "scenariofile/cpps_scenariofile.h"
 #include "utils/random_engine.h"
 #include "utils/sola_utils.h"
 
@@ -40,42 +42,11 @@ using namespace ns3;
 
 namespace daisi::cpps {
 
-// WIFI
-static void handleWifiMacTxDrop(ns3::Ptr<const ns3::Packet>) {
-  throw std::runtime_error("Packet was dropped (MacTxDrop at Wifi)!");
-}
-
-static void handleWifiPhyTxDrop(ns3::Ptr<const ns3::Packet>) {
-  throw std::runtime_error("Packet was dropped (PhyTxDrop at Wifi)!");
-}
-
-static void handleWifiMacRxDrop(ns3::Ptr<const ns3::Packet>) {
-  throw std::runtime_error("Packet was dropped (MacRxDrop at Wifi)!");
-}
-
-static void handleWifiDeassociated(ns3::Mac48Address) {
-  throw std::runtime_error("wifi connection lost (deassociated from access point)");
-}
-
-static void installDefaultWifiTraces() {
-  ns3::Config::ConnectWithoutContext("/NodeList/*/DeviceList/*/$ns3::WifiNetDevice/Mac/MacTxDrop",
-                                     MakeCallback(&handleWifiMacTxDrop));
-  ns3::Config::ConnectWithoutContext(
-      "/NodeList/*/DeviceList/*/$ns3::WifiNetDevice/Mac/$ns3::StaWifiMac/DeAssoc",
-      MakeCallback(&handleWifiDeassociated));
-  ns3::Config::ConnectWithoutContext("/NodeList/*/DeviceList/*/$ns3::WifiNetDevice/Mac/MacRxDrop",
-                                     MakeCallback(&handleWifiMacRxDrop));
-  ns3::Config::ConnectWithoutContext("/NodeList/*/DeviceList/*/$ns3::WifiNetDevice/Phy/PhyTxDrop",
-                                     MakeCallback(&handleWifiPhyTxDrop));
-}
-
 /// @brief Global ptr to AmrAssetConnector for ns-3 to pass mobility model
 extern ns3::Ptr<daisi::cpps::AmrMobilityModelNs3> next_mobility_model;
 
 CppsManager::CppsManager(const std::string &scenario_config_file)
-    : ManagerOld<CppsApplication>(scenario_config_file), scenario_(scenario_config_file) {
-  ManagerOld::initLogger();
-
+    : scenario_(scenario_config_file) {
   amr_descriptions_ = scenario_.getAmrDescriptions();
   material_flow_descriptions_ = scenario_.getMaterialFlowDescriptions();
 
@@ -120,26 +91,15 @@ void CppsManager::spawnAMR(uint32_t amr_index, const AmrDescription &description
           scenario_.algorithm.getParticipantAlgorithmConfig(), amr_index == 0);
 }
 
-void CppsManager::setup() {
+void CppsManager::setupImpl() {
   daisi::global_logger_manager->logMinhtonConfigFile("configurations/root.yml");
   daisi::global_logger_manager->logMinhtonConfigFile("configurations/join.yml");
 
   setupNodes();
 
-  // Setup AMRs
-  assert(this->node_container_.GetN() == getNumberOfNodes());
-
-  ns3::MobilityHelper mob;
-  mob.SetPositionAllocator("ns3::GridPositionAllocator", "MinX",
-                           ns3::DoubleValue(scenario_.topology.width * 0.2), "MinY",
-                           ns3::DoubleValue(scenario_.topology.height * 0.3), "DeltaX",
-                           ns3::DoubleValue(2.5), "DeltaY", ns3::DoubleValue(2), "GridWidth",
-                           ns3::UintegerValue(12), "LayoutType", ns3::StringValue("RowFirst"));
-
-  mob.SetMobilityModel("ns3::AmrMobilityModelNs3");
-  mob.Install(amrs_);
-
   initialSpawn();
+
+  scheduleEvents();
 }
 
 void CppsManager::initialSpawn() {
@@ -161,7 +121,7 @@ void CppsManager::initialSpawn() {
   }
 }
 
-uint64_t CppsManager::getNumberOfNodes() {
+uint64_t CppsManager::getNumberOfNodes() const {
   return scenario_.initial_number_of_amrs + scenario_.number_of_material_flow_agents;
 }
 
@@ -236,176 +196,39 @@ void CppsManager::startMF(uint32_t index) {
 void CppsManager::setupNodes() {
   amrs_.Create(scenario_.initial_number_of_amrs);
   material_flows_.Create(scenario_.number_of_material_flow_agents);
-  setupNetworkEthernet();
-  setupNetworkWifi();
 
-  node_container_ = NodeContainer(material_flows_, amrs_);
+  core_network_.addNodesCSMA(material_flows_);
+  core_network_.addNodesWifi(amrs_, scenario_.topology.width, scenario_.topology.height);
 
-  // Setting up TransportOrderApplications and one AMR application
-  setupApplication();
+  daisi::registerNodes(material_flows_);
+  daisi::registerNodes(amrs_);
 
-  // Setup second applications for physical AMR
-  for (uint32_t i = 0; i < amrs_.GetN(); i++) {
-    installApplication<CppsApplication>(this->amrs_.Get(i));
+  // Setup Applications
+  for (int i = 0; i < amrs_.GetN(); i++) {
+    // For logical AMR
+    installApplication<CppsApplication>(amrs_.Get(i));
+    amrs_.Get(i)->GetApplication(0)->SetStartTime(ns3::MilliSeconds(0));
+
+    // For physical AMR
+    installApplication<CppsApplication>(amrs_.Get(i));
+    amrs_.Get(i)->GetApplication(1)->SetStartTime(ns3::MilliSeconds(0));
   }
-}
 
-void CppsManager::setupNetworkEthernet() {
-  // Use the manager to set up ethernet between specific nodes only
-  // Rest of exclusive network setup for cpps is handled here.
-  node_container_ = NodeContainer(material_flows_);
+  for (int i = 0; i < material_flows_.GetN(); i++) {
+    installApplication<CppsApplication>(material_flows_.Get(i));
+    material_flows_.Get(i)->GetApplication(0)->SetStartTime(ns3::MilliSeconds(0));
+  }
 
-  setupNetwork();
-}
-
-void CppsManager::setupNetworkWifi() {
-  // Setup APs
-  const uint32_t number_ap = std::ceil(amrs_.GetN() / (double)constants::kMaxNumberAmrsPerAp);
-  access_points_.Create(number_ap);
-
-  // Install APs all in middle of topology
-  MobilityHelper mob;
+  // Install MobilityModel
+  ns3::MobilityHelper mob;
   mob.SetPositionAllocator("ns3::GridPositionAllocator", "MinX",
-                           DoubleValue(scenario_.topology.width / 2.0), "MinY",
-                           DoubleValue(scenario_.topology.height / 2.0), "Z", DoubleValue(8.0),
-                           "DeltaX", DoubleValue(0.0), "DeltaY", DoubleValue(0.0), "GridWidth",
-                           UintegerValue(20), "LayoutType", StringValue("RowFirst"));
-  mob.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+                           ns3::DoubleValue(scenario_.topology.width * 0.2), "MinY",
+                           ns3::DoubleValue(scenario_.topology.height * 0.3), "DeltaX",
+                           ns3::DoubleValue(2.5), "DeltaY", ns3::DoubleValue(2), "GridWidth",
+                           ns3::UintegerValue(12), "LayoutType", ns3::StringValue("RowFirst"));
 
-  mob.Install(access_points_);
-
-  YansWifiChannelHelper channel = YansWifiChannelHelper::Default();
-  YansWifiPhyHelper wifi_phy = YansWifiPhyHelper();
-  wifi_phy.SetPcapDataLinkType(YansWifiPhyHelper::DLT_IEEE802_11_RADIO);
-  channel.SetPropagationDelay("ns3::ConstantSpeedPropagationDelayModel");
-
-  // Assign amrs to AP
-  std::vector<NodeContainer> amrs_per_ap;
-  amrs_per_ap.resize(number_ap);
-  for (uint32_t i = 0; i < amrs_.GetN(); i++) {
-    amrs_per_ap[i / constants::kMaxNumberAmrsPerAp].Add(amrs_.Get(i));
-  }
-
-  const uint32_t base_address = 3232235520;  // 192.168.0.0
-
-  // Install internet
-  InternetStackHelper stack;
-  stack.Install(amrs_);
-
-  for (uint32_t i = 0; i < access_points_.GetN(); i++) {
-    wifi_phy.SetChannel(channel.Create());
-    WifiHelper wifi = WifiHelper();
-    wifi.SetRemoteStationManager("ns3::IdealWifiManager");
-    wifi.SetRemoteStationManager("ns3::IdealWifiManager", "RtsCtsThreshold", UintegerValue(100));
-    wifi.SetStandard(WifiStandard::WIFI_STANDARD_80211ac);
-
-    // As long as we do not use handover of an STA between APs, each
-    // STA is always attached to the same AP.
-    // As the number of STAs can be large and only a small number of STAs will be attached
-    // to a single AP, we need many APs. Hence we (unrealistically) increase the frequency
-    // for every AP by 1. This works as ns-3 does not model radio interferences yet.
-    wifi_phy.Set("Frequency", UintegerValue(i + 1));
-    wifi_phy.Set("ChannelWidth", UintegerValue(20));
-    WifiMacHelper wifi_mac;
-    Ssid ssid = Ssid("IMLwifi" + std::to_string(i));
-    wifi_mac.SetType("ns3::StaWifiMac", "Ssid", SsidValue(ssid), "ActiveProbing",
-                     BooleanValue(false), "WaitBeaconTimeout", TimeValue(Seconds(2.56)));
-
-    // Setup wifi on AMR
-    NetDeviceContainer amr_sta_dev_temp;
-    wifi_phy.Set("TxPowerStart", DoubleValue(constants::kMaxPowerDBm));
-    wifi_phy.Set("TxPowerEnd", DoubleValue(constants::kMaxPowerDBm));
-    amr_sta_dev_temp = wifi.Install(wifi_phy, wifi_mac, amrs_per_ap[i]);
-    amr_sta_dev_.Add(amr_sta_dev_temp);
-
-    // set up the AP
-    bool beacon_generation = true;
-#ifdef DAISI_SOLANET_NS3_DISABLE_NETWORKING
-    beacon_generation = false;
-#endif
-
-    wifi_mac.SetType("ns3::ApWifiMac", "QosSupported", BooleanValue(true), "Ssid", SsidValue(ssid),
-                     "BeaconInterval", TimeValue(Seconds(2.56)), "BeaconGeneration",
-                     ns3::BooleanValue(beacon_generation));
-    auto ap_sta_dev_temp = wifi.Install(wifi_phy, wifi_mac, access_points_.Get(i));
-
-    // Connection between AP and core router
-    CsmaHelper csma;
-    csma.SetChannelAttribute("DataRate", StringValue("10Gbps"));
-    // Speed of light delay
-    // https://en.wikipedia.org/wiki/Velocity_factor#Typical_velocity_factors
-    // assuming 10cm/optical fiber: 10cm/0.67c = 498ps
-    csma.SetChannelAttribute("Delay", TimeValue(PicoSeconds(498)));
-    auto link = csma.Install(NodeContainer(access_points_.Get(i), core_router_.Get(0)));
-
-    link.Get(0)->TraceConnectWithoutContext("MacTxDrop", MakeCallback(&handleSwitchMacTxDrop));
-    link.Get(0)->TraceConnectWithoutContext("PhyRxDrop", MakeCallback(&handleSwitchPhyRxDrop));
-    link.Get(0)->TraceConnectWithoutContext("PhyTxDrop", MakeCallback(&handleSwitchPhyTxDrop));
-    link.Get(1)->TraceConnectWithoutContext("MacTxDrop", MakeCallback(&handleRouterMacTxDrop));
-    link.Get(1)->TraceConnectWithoutContext("PhyRxDrop", MakeCallback(&handleRouterPhyRxDrop));
-    link.Get(1)->TraceConnectWithoutContext("PhyTxDrop", MakeCallback(&handleRouterPhyTxDrop));
-
-    // Bridge between cable and wireless on AP
-    BridgeHelper bridge;
-    bridge.Install(access_points_.Get(i), ns3::NetDeviceContainer(ap_sta_dev_temp, link.Get(0)));
-
-    // Assign IPs
-    Ipv4AddressHelper ip;
-    const uint32_t ip_address = base_address + 65536 * i;
-    ip.SetBase(Ipv4Address(ip_address), "255.255.0.0");
-    auto router_ip = ip.Assign(link.Get(1));
-    auto sta_interface = ip.Assign(amr_sta_dev_temp);
-
-    ns3::Ipv4InterfaceContainer ips;
-    ips.Add(router_ip);
-    ips.Add(sta_interface);
-    ip_container_.push_back(ips);
-
-    // Add routing
-    uint32_t port_on_router = switchContainer_.GetN() + i;
-
-    // Add AP as destination for routing to all other router ports
-    Ipv4StaticRoutingHelper ipv4_routing_helper;
-    for (uint32_t j = 0; j < ip_container_.size() - 1; j++) {
-      Ptr<Ipv4StaticRouting> static_routing =
-          ipv4_routing_helper.GetStaticRouting(ip_container_[j].Get(0).first);
-      static_routing->AddNetworkRouteTo(
-          Ipv4Address(ip_address), "255.255.0.0",
-          port_on_router + 1);  // +1 to not send on loopback (index=0)
-    }
-
-    // Setup app routing
-    for (uint32_t j = 1; j < ip_container_[ip_container_.size() - 1].GetN(); j++) {
-      auto static_routing = ipv4_routing_helper.GetStaticRouting(
-          ip_container_[ip_container_.size() - 1].Get(j).first);
-
-      // Default route/gateways
-      auto current_router_ip = ip_container_[ip_container_.size() - 1].GetAddress(0);
-      static_routing->AddNetworkRouteTo(Ipv4Address("0.0.0.0"), "0.0.0.0", current_router_ip, 1);
-    }
-
-    // set up ARP caches for AMR and core router
-    // Might not work for Wi-Fi devices.
-    // See https://gitlab.com/nsnam/ns-3-dev/-/issues/664
-    NetDeviceContainer container(link.Get(1), amr_sta_dev_temp);
-    assert(ips.GetN() == container.GetN());
-    for (int j = 0; j < container.GetN(); j++) {
-      auto ip_j = ips.Get(j);
-      auto arpcache =
-          ip_j.first->GetObject<Ipv4L3Protocol>()->GetInterface(ip_j.second)->GetArpCache();
-
-      for (int k = 0; k < ips.GetN(); k++) {
-        if (j == k) continue;
-        auto other_ip = ips.GetAddress(k);
-        auto other_mac = container.Get(k)->GetAddress();
-        ns3::ArpCache::Entry *arp_entry = arpcache->Add(other_ip);
-        arp_entry->SetMacAddress(Mac48Address::ConvertFrom(other_mac));
-        arp_entry->MarkPermanent();
-      }
-    }
-  }
-
-  installDefaultWifiTraces();
+  mob.SetMobilityModel("ns3::AmrMobilityModelNs3");
+  mob.Install(amrs_);
 }
 
 void CppsManager::executeMaterialFlow(int index, const std::string & /*friendly_name*/) {
@@ -545,7 +368,7 @@ void CppsManager::scheduleEvents() {
   }
 }
 
-std::string CppsManager::getDatabaseFilename() {
+std::string CppsManager::getDatabaseFilename() const {
   return generateDBName("cpps", generateRandomString());
 }
 
